468d1746fe7971f0e79815351a3c09338b553063
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-13 21:58:35 +0900

    Fixed documentation

---
+++ modipyd/__init__.py
@@ -0,0 +1,16 @@
+"""
+modipyd
+================================================
+
+How this software works:
+
+1. Generating python module dependency graphs
+2. Monitoring module (file) modification
+3. Notifying modified and dependent modules
+
+So I named it modipyd (modified + python).
+
+    :copyright: 2008 by Takanori Ishikawa <takanori.ishikawa@gmail.com>
+    :license: MIT (See ``LICENSE`` file for more details)
+
+"""

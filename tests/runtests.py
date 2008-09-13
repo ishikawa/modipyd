27574763f509fb8bda3947fd236839735ce3cfc2
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-13 22:36:45 +0900

    Added test script

---
+++ tests/runtests.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+"""
+Regression test for modipyd.
+"""
+
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+import modipyd
+
+if __name__ == '__main__':
+    pass

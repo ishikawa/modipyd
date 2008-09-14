4316c4d9bed7bbc5f578961ddc50674a92f52cdd
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-15 01:08:56 +0900

    Rename method

--- tests/runtests.py
+++ tests/runtests.py
@@ -26,7 +26,7 @@ class TestModipydCollectFiles(unittest.TestCase):
         self.assert_(os.path.exists(FILES_DIR))
         self.assert_(os.path.isdir(FILES_DIR))
 
-    def test_collect_files_not_found(self):
+    def test_not_found(self):
         filename = os.path.join(FILES_DIR, 'no file or directory')
         self.assert_(not os.path.exists(filename))
 

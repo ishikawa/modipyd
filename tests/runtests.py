e73013d854627c19bb5319c12eb54364fdcd91a7
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-14 20:48:04 +0900

    Added 'Developer Notes' section

--- tests/runtests.py
+++ tests/runtests.py
@@ -11,11 +11,20 @@ import unittest
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import modipyd
 
+# ----------------------------------------------------------------
+# Settings
+# ----------------------------------------------------------------
+# The directory contains test files.
+FILES_DIR = os.path.join(os.path.dirname(__file__))
+
 
 class TestModipyd(unittest.TestCase):
     """Tests modipyd functionalities"""
 
-    pass
+    def test_files_dir_exists(self):
+        self.assert_(os.path.exists(FILES_DIR))
+
+
 
 if __name__ == '__main__':
     unittest.main()

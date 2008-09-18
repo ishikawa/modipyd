a1e1f84b9877460c9c3e0e20d238dab3024bf79c
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-18 16:40:18 +0900

    Added test_different_modulename

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,12 +24,20 @@ class TestMakeModulename(unittest_helper.TestCase):
         r = TestMakeModulename.MODULE_NAME_RE
         self.assert_(r.match(utils.make_modulename(s)))
 
+    def assert_different_modulename(self, s1, s2):
+        modname1 = utils.make_modulename(s1)
+        modname2 = utils.make_modulename(s2)
+        self.assertNotEqual(modname1, modname2)
+
     def test_make_modulename(self):
         self.assert_modulename("")
         self.assert_modulename("A")
         self.assert_modulename("/")
         self.assert_modulename("/usr/bin/python")
         self.assert_modulename("/usr/bin/test.py")
+
+    def test_different_modulename(self):
+        self.assert_different_modulename("", "A")
 
 
 if __name__ == '__main__':

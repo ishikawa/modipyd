f72f79fd9bb640ae3fe461ae9c656df16516faec
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-20 12:07:21 +0900

    Using detect_modulename

--- tests/runtests.py
+++ tests/runtests.py
@@ -8,8 +8,12 @@ import os
 import unittest
 import imp
 from os.path import join, dirname, basename, splitext
+from modipyd.utils import detect_modulename
 
 tests = unittest.TestSuite()
+
+#import sys, pprint
+#pprint.pprint(sys.path)
 
 for dirpath, dirnames, filenames in os.walk(dirname(__file__)):
     for name in filenames:
@@ -17,8 +21,8 @@ for dirpath, dirnames, filenames in os.walk(dirname(__file__)):
             continue
 
         filepath = join(dirpath, name)
-        modulename, _ = splitext(basename(filepath))
-
+        modulename = detect_modulename(filepath)
+        #print modulename
         module = imp.load_source(modulename, filepath)
         suite = unittest.defaultTestLoader.loadTestsFromModule(module)
         tests.addTest(suite)

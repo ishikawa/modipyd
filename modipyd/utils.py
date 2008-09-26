43df57569b6112026ef130230e99f1025b905983
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-26 18:56:11 +0900

    Added doctest

--- modipyd/utils.py
+++ modipyd/utils.py
@@ -14,6 +14,17 @@ def wrap_sequence(obj, sequence_type=tuple):
     """
     Return a tuple whose item is obj.
     If obj is already a list or tuple, it is returned unchanged.
+
+    >>> wrap_sequence(None)[0] is None
+    True
+    >>> wrap_sequence("")
+    ('',)
+    >>> wrap_sequence([])
+    []
+    >>> wrap_sequence(())
+    ()
+    >>> wrap_sequence(123, sequence_type=list)
+    [123]
     """
     if isinstance(obj, (list, tuple)):
         return obj

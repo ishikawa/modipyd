6a48913dfcc9edcbd5a1fc3807e84012bb2ec0bc
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-27 21:51:00 +0900

    Added test_start_iterator

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -13,6 +13,13 @@ class TestModipydMonitor(TestCase):
         self.assertNotNone(monitor)
         self.assert_(not monitor.monitoring)
 
+    def test_start_iterator(self):
+        monitor = Monitor(join(FILES_DIR, 'cycles'), [FILES_DIR])
+        modified_iter = monitor.start()
+        self.assertNotNone(modified_iter)
+        self.assert_(hasattr(modified_iter, 'next'))
+        self.assert_(callable(modified_iter.next))
+
 
 if __name__ == '__main__':
     unittest.main()

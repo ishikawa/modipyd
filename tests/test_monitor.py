454fe7d7b45ebd1a4f9f42fd4aefa4efd2205613
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-27 21:11:42 +0900

    Added test_monitor

---
+++ tests/test_monitor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import unittest
+from tests import TestCase
+from modipyd.monitor import Monitor
+
+
+class TestModipydMonitor(TestCase):
+
+    def test_init(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

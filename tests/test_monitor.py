6ca669c87a68bdf91339b32da28d2b82fa146fb7
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-28 00:56:59 +0900

    Added test_modified

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import unittest
+import time
 from os.path import join, exists
 
 from tests import TestCase, FILES_DIR
@@ -30,15 +31,61 @@ PRISONERS_DIR = join(FILES_DIR, 'prisoners')
 
 class TestMonitor(TestCase):
 
-    def setUp(self):
-        path = join(PRISONERS_DIR, '__init__.py')
-        if not exists(path):
-            f = open(path, 'w')
-            f.write("")
+    def mkfiles(self):
+        # __init__.py
+        f = open(join(PRISONERS_DIR, '__init__.py'), 'w')
+        try:
+            f.write("from prisoners.b import money")
+        finally:
             f.close()
 
+        # a.py
+        f = open(join(PRISONERS_DIR, 'a.py'), 'w')
+        try:
+            f.write("")
+        finally:
+            f.close()
+
+        # b.py
+        f = open(join(PRISONERS_DIR, 'b.py'), 'w')
+        try:
+            f.write("""\
+import prisoners.a
+money = 4321.09
+""")
+        finally:
+            f.close()
+
+    def setUp(self):
+        self.mkfiles()
+        self.monitor = Monitor(PRISONERS_DIR, [FILES_DIR])
+
     def test_init(self):
-        pass
+        self.assertNotNone(self.monitor)
+        descriptors = self.monitor.descriptors
+        self.assertNotNone(self.monitor.descriptors)
+        self.assertEqual(3, len(descriptors))
+        self.assert_('prisoners' in descriptors)
+        self.assert_('prisoners.a' in descriptors)
+        self.assert_('prisoners.b' in descriptors)
+
+    def test_modified(self):
+        modified = self.monitor.monitor()
+        self.assertEqual(0, len(modified))
+        time.sleep(1.5)
+
+        # modify
+        f = open(join(PRISONERS_DIR, 'b.py'), 'w')
+        f.write("")
+        f.close()
+
+        modified = self.monitor.monitor()
+        self.assertEqual(1, len(modified))
+        m = modified[0]
+        self.assertEqual('prisoners.b', m.name)
+        self.assertEqual(0, len(m.dependencies))
+        self.assertEqual(1, len(m.reverse_dependencies))
+
 
 
 if __name__ == '__main__':

375e534dbbf87ac32f2db7792fc5d76347ca8f60
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-09-30 19:32:18 +0900

    Added gather_tests()

--- tests/runtests.py
+++ tests/runtests.py
@@ -13,33 +13,37 @@ from modipyd.utils import resolve_modulename, import_module
 from modipyd.module import collect_module_code
 
 
-tests = unittest.TestSuite()
+def gather_tests():
+    """Returns unittest.TestSuite instance"""
+    tests = unittest.TestSuite()
 
-# unittest
-for dirpath, dirnames, filenames in os.walk(dirname(__file__)):
-    if 'files' in dirnames:
-        dirnames.remove('files')
+    # unittest
+    for dirpath, dirnames, filenames in os.walk(dirname(__file__)):
+        if 'files' in dirnames:
+            dirnames.remove('files')
 
-    for name in filenames:
-        if not (name.startswith("test_") and name.endswith(".py")):
-            continue
+        for name in filenames:
+            if not (name.startswith("test_") and name.endswith(".py")):
+                continue
 
-        filepath = join(dirpath, name)
-        modulename = resolve_modulename(filepath)
-        module = import_module(modulename)
-        suite = unittest.defaultTestLoader.loadTestsFromModule(module)
-        tests.addTest(suite)
+            filepath = join(dirpath, name)
+            modulename = resolve_modulename(filepath)
+            module = import_module(modulename)
+            suite = unittest.defaultTestLoader.loadTestsFromModule(module)
+            tests.addTest(suite)
 
-# doctest
-for modcode in collect_module_code(join(dirname(__file__), '..', 'modipyd')):
-    module = import_module(modcode.name)
-    try:
-        suite = doctest.DocTestSuite(module)
-    except ValueError:
-        pass
-    else:
-        tests.addTest(suite)
+    # doctest
+    for modcode in collect_module_code(join(dirname(__file__), '..', 'modipyd')):
+        module = import_module(modcode.name)
+        try:
+            suite = doctest.DocTestSuite(module)
+        except ValueError:
+            pass
+        else:
+            tests.addTest(suite)
+
+    return tests
 
 
 if __name__ == '__main__':
-    unittest.TextTestRunner().run(tests)
+    unittest.TextTestRunner().run(gather_tests())

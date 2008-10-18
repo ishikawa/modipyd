e0e49779668d7059a1e78d116eecd6e418baa64e
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2008-10-18 09:22:44 +0900

    Fixed documentation

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -44,7 +44,7 @@ copyright = '2008, Takanori Ishikawa'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0rc1'
+release = '1.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
